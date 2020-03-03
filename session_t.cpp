4894330b1448fae8df8a971ba58cf1a5e87df6bb
Michael L. Collard, Ph.D <collard@uakron.edu>
2020-03-03 15:49:17 -0500

    Add Clock and TimeClock

--- session_t.cpp
+++ session_t.cpp
@@ -8,6 +8,17 @@
 #include <unistd.h>
 #include <chrono>
 
+class Clock {
+public:
+    virtual std::time_t start() const = 0;
+    virtual std::time_t now() const = 0;
+};
+
+class TimeClock : public Clock {
+    virtual std::time_t start() const { return now(); }
+    virtual std::time_t now() const { return std::time(nullptr); }
+};
+
 class Session {
 public:
     Session() : start_time(std::chrono::system_clock::now()) {}
@@ -26,6 +37,14 @@ int main() {
         sleep(2);
         s.stop();
         assert(s.seconds() == 2);
+    }
+
+    // 10-second session
+    {
+        Session s;
+        sleep(10);
+        s.stop();
+        assert(s.seconds() == 10);
     }
 
     return 0;
