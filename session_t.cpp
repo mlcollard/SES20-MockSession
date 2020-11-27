971250577287b4fc5e3e4f753d11c23763a0e25a
Michael L. Collard, Ph.D <collard@uakron.edu>
2020-11-27 11:55:52 -0500

    Add in class part

--- session_t.cpp
+++ session_t.cpp
@@ -15,21 +15,52 @@ public:
 };
 
 class TimeClock : public Clock {
+public:
     virtual std::time_t start() const { return now(); }
     virtual std::time_t now() const { return std::time(nullptr); }
 };
 
+class TenSecondClock : public Clock {
+public:
+    virtual std::time_t start() const { return 0; }
+    virtual std::time_t now() const { return 10; }
+};
+
+class TenMinuteClock : public Clock {
+public:
+    virtual std::time_t start() const { return 0; }
+    virtual std::time_t now() const { return 10 * 60; }
+};
+
+class ConfigurableClock : public Clock {
+public:
+    ConfigurableClock(std::time_t amount) : length(amount) {}
+    virtual std::time_t start() const { return 0; }
+    virtual std::time_t now() const { return length; }
+private:
+    std::time_t length;
+};
+
 class Session {
 public:
-    Session() : start_time(std::chrono::system_clock::now()) {}
-    void stop() { stop_time = std::chrono::system_clock::now(); }
-    int seconds() { return std::chrono::duration_cast<std::chrono::seconds>(stop_time - start_time).count(); }
+    Session(const Clock& clock = TimeClock()) : clock(clock), start_time(clock.start()) {}
+    void stop() { stop_time = clock.now(); }
+    std::time_t seconds() { return stop_time - start_time; }
 private:
-    std::chrono::system_clock::time_point start_time;
-    std::chrono::system_clock::time_point stop_time;
+    const Clock& clock;
+    std::time_t start_time;
+    std::time_t stop_time;
 };
 
 int main() {
+
+    // 10-minute session
+    {
+        ConfigurableClock clock(10 * 60);
+        Session s(clock);
+        s.stop();
+        assert(s.seconds() == 10 * 60);
+    }
 
     // 2-second session
     {
@@ -41,8 +72,8 @@ int main() {
 
     // 10-second session
     {
-        Session s;
-        sleep(10);
+        TenSecondClock clock;
+        Session s(clock);
         s.stop();
         assert(s.seconds() == 10);
     }
